ba3acc6535c25b06e2be2914cc6c0d0d450f7e15
Jeakith <ralfs.naglinskis@inbox.lv>
2023-02-20 18:26:05 +0200

    added login

---
+++ login2.js
@@ -0,0 +1,12 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="Jeakith" && password=="PreiliTop123")
+{
+window.open("index.html")
+}
+else{
+    alert("Login Failed")
+}
+}
